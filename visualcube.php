71ec5472576915355ac8071d60ed6f78ca9e90b4
cride5 <cride5@749fe6d1-1c54-4171-aaf7-8a91365e0645>
2010-04-02 14:00:32 +0000

    Incremented version number to 0.3.1

--- visualcube.php
+++ visualcube.php
@@ -54,7 +54,7 @@
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<title>VisualCube (v0.3.0)</title>
+		<title>VisualCube (v0.3.1)</title>
 		<meta name="description"        content="Rubiks cube visualiser"/>
 		<meta name="keywords"           content="visualcube, visual cube, imagecube, image cube, cube vis, viscube, visual rubiks, imgcube, cube image, cube gif, cub png, cube jpeg"/>
 		<meta name="resource-type"      content="document"/>
